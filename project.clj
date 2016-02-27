b385d6835e92601616cbd8c7c90f27509bab1da5
selfsame <joseph.parker.1@gmail.com>
2016-02-26 19:27:57 -0500

    initial commit

---
+++ project.clj
@@ -0,0 +1,48 @@
+(defproject ipjs "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :min-lein-version "2.5.3"
+  
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.170"]
+                 [org.clojure/core.async "0.2.374"
+                  :exclusions [org.clojure/tools.reader]]]
+  
+  :plugins [[lein-figwheel "0.5.0-3"]
+            [lein-cljsbuild "1.1.2" :exclusions [[org.clojure/clojure]]]]
+
+  :source-paths ["src"]
+
+  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
+
+  :cljsbuild {:builds
+              [{:id "dev"
+                :source-paths ["src"]
+
+                ;; If no code is to be run, set :figwheel true for continued automagical reloading
+                :figwheel {:on-jsload "ipjs.core/on-js-reload"}
+
+                :compiler {:main ipjs.core
+                           :asset-path "js/compiled/out"
+                           :output-to "resources/public/js/compiled/ipjs.js"
+                           :output-dir "resources/public/js/compiled/out"
+                           :source-map-timestamp true}}
+               ;; This next build is an compressed minified build for
+               ;; production. You can build this with:
+               ;; lein cljsbuild once min
+               {:id "min"
+                :source-paths ["src"]
+                :compiler {:output-to "resources/public/js/compiled/ipjs.js"
+                           :main ipjs.core
+                           :optimizations :advanced
+                           :pretty-print false}}]}
+
+  :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
+             ;; :server-port 3449 ;; default
+             ;; :server-ip "127.0.0.1"
+             :load-warninged-code true
+             :css-dirs ["resources/public/css"] ;; watch and update CSS
+             })
